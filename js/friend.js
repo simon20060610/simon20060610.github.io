4ea3e8b5bdb119729b8751d9272f820618835887
Simon <ziyi_chen@stu.cdut.edu.cn>
2026-06-19 02:31:23 +0800

    Site updated: 2026-06-19 02:31:19

--- js/friend.js
+++ js/friend.js
@@ -1,5 +1,4 @@
-// author by removef
-// https://removeif.github.io/
+// Friend links renderer.
 $(function () { //获取处理友链数据
     $.getJSON("../json_data/friend.json", function (data) {
 
@@ -11,7 +10,7 @@ $(function () { //获取处理友链数据
         data = data.filter((item, a, b) => item.valid != 0).sort(function (a, b) {
             return Math.random() > .5 ? -1 : 1;
         });
-        $('.links-content').append("<div class='friend-title-item'><br>大佬们<br><br><hr></div>");
+        $('.links-content').append("<div class='friend-title-item'><br>友情链接<br><br><hr></div>");
         $.each(data, function (i, e) {
             var html = "<div class=\"friend-card-item\">";
             if (e.src == undefined) {
@@ -30,7 +29,7 @@ $(function () { //获取处理友链数据
 
         // 过期的
         if (notValid.length > 0) {
-            $('.links-content').append("<div class='friend-title-item'><br>异常的大佬们<br><br><hr></div></div>");
+            $('.links-content').append("<div class='friend-title-item'><br>暂不可访问<br><br><hr></div></div>");
             $.each(notValid, function (i, e) {
                 var html = "<div class=\"friend-card-item\">";
                 html += "    <img class=\"ava\" src=\"/img/links/nopic.jpg\" title=\"图片链接不可用，使用的默认图片\">";
